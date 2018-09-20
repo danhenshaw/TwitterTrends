6f4c30cca1a8c1dabad44f4029640feaaeae4eed
Dan Henshaw <danieljhenshaw@gmail.com>
2018-09-20 09:22:25 +0200

    Initial Commit

---
+++ TwitterTrends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterTrends
+//
+//  Created by Daniel Henshaw on 20/9/18.
+//  Copyright © 2018 Dan Henshaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
